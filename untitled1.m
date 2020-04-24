9776f764c4d25b92b06aae198242395ed74390e4
jinjerrbread <ionirinafrancisca@gmail.com>
2020-04-24 11:04:13 +0300

    irina

---
+++ untitled1.m
@@ -0,0 +1,154 @@
+function varargout = untitled1(varargin)
+% UNTITLED1 MATLAB code for untitled1.fig
+%      UNTITLED1, by itself, creates a new UNTITLED1 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED1 returns the handle to a new UNTITLED1 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED1.M with the given input arguments.
+%
+%      UNTITLED1('Property','Value',...) creates a new UNTITLED1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled1
+
+% Last Modified by GUIDE v2.5 24-Apr-2020 01:47:36
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled1_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled1 is made visible.
+function untitled1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled1 (see VARARGIN)
+
+% Choose default command line output for untitled1
+handles.output = hObject;
+
+
+
+ah=axes('unit','normalized','position',[0 0 1 1]);
+bg=imread('/Users/irinafranciscaion/Desktop/wallies.jpg'); imagesc(bg);
+%set(ah,'handlesvisibility','off','visible','off')
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fs=8000;
+nbits=16;
+nchannels=1;
+duration=6;
+handles.arObj=audiorecorder(fs, nbits, nchannels);
+recordblocking(handles.arObj, duration);
+handles.y=getaudiodata(handles.arObj);
+ms2 = fs/500;
+
+ms20 = fs/50;
+
+r = xcorr(handles.y, ms20, 'coeff');
+
+d = (-ms20:ms20)/fs;
+
+%plot(d, r);
+
+%title('Autocorrelation');
+
+%xlabel('Delay (s)');
+
+%ylabel('Correlation coeff.');
+
+r = r(ms20 + 1 : 2*ms20+1);
+
+[rmax, tx] = max(r(ms2:ms20));
+
+ Fx = fs/(ms2+tx-1);
+
+ %%?-recognizing voice?
+
+Fth= 160; %% threshold frequency is 160 Hz, you can change this frequency too
+
+if Fx> Fth
+
+    set(handles.edit1,'String','Hello,I see you are a Miss. I will call you Darlene.Rememeber the last letter of your name, you will need it later');
+else
+
+    set(handles.edit1,'String','Hello, I see I am talking to a Mr. I will call you Elliot.Rememeber the first letter of your name, you will need it later!');
+
+end
+guidata(hObject, handles);
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit1 as text
+%        str2double(get(hObject,'String')) returns contents of edit1 as a double
+
+
+
+
+
+% --- Executes during object creation, after setting all properties.
+function edit1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
