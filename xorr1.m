ff88de2abcb91c3a2c5ef74d59547b9e9db60bfe
jinjerrbread <ionirinafrancisca@gmail.com>
2020-04-25 19:29:26 +0300

    irina

--- xorr1.m
+++ xorr1.m
@@ -1,14 +1,14 @@
 function[intermed,interm_key,cipher]=xorr1(input,key)
 
 
-    intermed=dec2bin(double(input));
-    interm_key=dec2bin(double(key));
+    intermed=dec2bin(double(input))
+    interm_key=dec2bin(double(key))
     if(length(input)>length(key))
-        x=length(input)-length(key);
-        interm_key=repmat(interm_key,x,1);
+        x=length(input)-length(key)+1
+        interm_key=repmat(interm_key,x,1)
         interm_key=interm_key(1:length(input),:)
     end
-    [m,n]=size(intermed);
+    [m,n]=size(intermed)
     for i=1:m
         for j=1:7
         if(intermed(i,j) ~= interm_key(i,j))
